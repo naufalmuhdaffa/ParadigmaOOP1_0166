92844be46b79ba202b19cf93bd02baa0ca823dc6
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-21 11:53:54 +0700

    menambahkan project baru, menambahkan obyek.

--- mainB/mainB.cpp
+++ mainB/mainB.cpp
@@ -7,5 +7,25 @@ using namespace std;
 
 int main()
 {
-    
+    ibu* varIbu = new ibu("Dini");
+    ibu* varibu2 = new ibu("Novi");
+    anak* varAnak1 = new anak("Tono");
+    anak* varAnak2 = new anak("Rini");
+    anak* varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varibu2->cetakAnak();
+
+    delete varIbu;
+    delete varibu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    return 0;
 }
